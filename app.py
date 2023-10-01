e2eaf4ede0bf188fc64c6248f93257130a292d8d
shivam <67598248+shivamshi@users.noreply.github.com>
2023-10-01 15:29:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -223,6 +223,7 @@ st.markdown("""
 
 
 st.markdown("""
+<br><br><br>
 <p align="center"><a href="https://www.buymeacoffee.com/cvcvcvcvcv"> 
     <img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" 
     height="50" width="210" alt="cvcvcvcvcv" />
