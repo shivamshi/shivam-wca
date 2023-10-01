6c5f69539ff10ac7060da263b9afa67d232090ba
shivam <67598248+shivamshi@users.noreply.github.com>
2023-10-01 14:20:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,23 @@ import helper
 import matplotlib.pyplot as plt
 import seaborn as sns
 import base64  # Import base64 for encoding the file
+import webbrowser  # Import webbrowser for opening the link
 
 st.sidebar.title("Whatsapp Chat Analyzer")
+
+# Add a download button for RandomChat.txt (visible at all times)
+if st.sidebar.button("Download RandomChat.txt"):
+    # Create a text content for the file
+    text_content = "This is the content of RandomChat.txt."
+    
+    # Set up the download link
+    href = f"data:text/plain;base64,{base64.b64encode(text_content.encode()).decode()}"
+    
+    # Add a download link to the page
+    st.sidebar.markdown(
+        f'<a href="{href}" download="RandomChat.txt">Click here to download RandomChat.txt</a>',
+        unsafe_allow_html=True
+    )
 
 uploaded_file = st.sidebar.file_uploader("Choose a file")
 if uploaded_file is not None:
@@ -56,13 +71,13 @@ if uploaded_file is not None:
         st.pyplot(fig)
 
         st.title('Activity Map')
-        col1,col2 = st.beta_columns(2)
+        col1, col2 = st.beta_columns(2)
 
         with col1:
             st.header("Most busy day")
-            busy_day = helper.week_activity_map(selected_user,df)
-            fig,ax = plt.subplots()
-            ax.bar(busy_day.index,busy_day.values,color='purple')
+            busy_day = helper.week_activity_map(selected_user, df)
+            fig, ax = plt.subplots()
+            ax.bar(busy_day.index, busy_day.values, color='purple')
             plt.xticks(rotation='vertical')
             st.pyplot(fig)
 
@@ -70,26 +85,26 @@ if uploaded_file is not None:
             st.header("Most busy month")
             busy_month = helper.month_activity_map(selected_user, df)
             fig, ax = plt.subplots()
-            ax.bar(busy_month.index, busy_month.values,color='orange')
+            ax.bar(busy_month.index, busy_month.values, color='orange')
             plt.xticks(rotation='vertical')
             st.pyplot(fig)
 
         st.title("Weekly Activity Map")
-        user_heatmap = helper.activity_heatmap(selected_user,df)
-        fig,ax = plt.subplots()
+        user_heatmap = helper.activity_heatmap(selected_user, df)
+        fig, ax = plt.subplots()
         ax = sns.heatmap(user_heatmap)
         st.pyplot(fig)
 
-        # finding the busiest users in the group(Group level)
+        # finding the busiest users in the group (Group level)
         if selected_user == 'Overall':
             st.title('Most Busy Users')
-            x,new_df = helper.most_busy_users(df)
+            x, new_df = helper.most_busy_users(df)
             fig, ax = plt.subplots()
 
             col1, col2 = st.beta_columns(2)
 
             with col1:
-                ax.bar(x.index, x.values,color='red')
+                ax.bar(x.index, x.values, color='red')
                 plt.xticks(rotation='vertical')
                 st.pyplot(fig)
             with col2:
@@ -97,45 +112,31 @@ if uploaded_file is not None:
 
         # WordCloud
         st.title("Wordcloud")
-        df_wc = helper.create_wordcloud(selected_user,df)
-        fig,ax = plt.subplots()
+        df_wc = helper.create_wordcloud(selected_user, df)
+        fig, ax = plt.subplots()
         ax.imshow(df_wc)
         st.pyplot(fig)
 
         # most common words
-        most_common_df = helper.most_common_words(selected_user,df)
+        most_common_df = helper.most_common_words(selected_user, df)
 
-        fig,ax = plt.subplots()
+        fig, ax = plt.subplots()
 
-        ax.barh(most_common_df[0],most_common_df[1])
+        ax.barh(most_common_df[0], most_common_df[1])
         plt.xticks(rotation='vertical')
 
-        st.title('Most commmon words')
+        st.title('Most common words')
         st.pyplot(fig)
 
         # emoji analysis
-        emoji_df = helper.emoji_helper(selected_user,df)
+        emoji_df = helper.emoji_helper(selected_user, df)
         st.title("Emoji Analysis")
 
-        col1,col2 = st.beta_columns(2)
+        col1, col2 = st.beta_columns(2)
 
         with col1:
             st.dataframe(emoji_df)
         with col2:
-            fig,ax = plt.subplots()
-            ax.pie(emoji_df[1].head(),labels=emoji_df[0].head(),autopct="%0.2f")
+            fig, ax = plt.subplots()
+            ax.pie(emoji_df[1].head(), labels=emoji_df[0].head(), autopct="%0.2f")
             st.pyplot(fig)
-
-        # Add a download button for RandomChat.txt
-        if st.sidebar.button("Download RandomChat.txt"):
-            # Create a text content for the file
-            text_content = "This is the content of RandomChat.txt."
-
-            # Set up the download link
-            href = f"data:text/plain;base64,{base64.b64encode(text_content.encode()).decode()}"
-
-            # Add a download link to the page
-            st.sidebar.markdown(
-                f'<a href="{href}" download="RandomChat.txt">Click here to download RandomChat.txt</a>',
-                unsafe_allow_html=True
-            )
